1aac4f2e911d753e03f66a6bc04f5e88ec05ef85
NenavizhyProekt <ostrouhov2013@gmail.com>
2024-12-17 15:03:31 +0300

    Удаление комментов

--- DynamicLib/DynamicLib/ArrayProcessor.h
+++ DynamicLib/DynamicLib/ArrayProcessor.h
@@ -10,15 +10,15 @@
 
 class DLL_API ArrayProcessor {
 public:
-    // —татический метод сортировки массива (пузырькова€ сортировка)
+    
     static void bubbleSort(int arr[], int size);
 
-    // —татический метод поиска элемента в массиве (линейный поиск)
+    
     static int findElement(int arr[], int size, int element);
 
-    // —татический метод нахождени€ среднего значени€
+    
     static double findAverage(int arr[], int size);
 
 };
 
-#endif // ARRAY_PROCESSOR_H
+#endif
--- DynamicLib/DynamicLib/dllmain.cpp
+++ DynamicLib/DynamicLib/dllmain.cpp
@@ -1,12 +1,11 @@
 ﻿#include "pch.h"
 #include "ArrayProcessor.h"
 
-// Реализация статических методов класса
+
 void ArrayProcessor::bubbleSort(int arr[], int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Обмен элементов
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -18,10 +17,10 @@ void ArrayProcessor::bubbleSort(int arr[], int size) {
 int ArrayProcessor::findElement(int arr[], int size, int element) {
     for (int i = 0; i < size; i++) {
         if (arr[i] == element) {
-            return i; // Возвращаем индекс найденного элемента
+            return i; 
         }
     }
-    return -1; // Элемент не найден
+    return -1;
 }
 
 double ArrayProcessor::findAverage(int arr[], int size) {
